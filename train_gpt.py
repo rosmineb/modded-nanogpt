3a3fc94c8130400a23bd6bcfe98fb6cb69ba11fa
rosmine <rosmine@rosmine.ai>
2025-03-25 18:57:37 +0000

    add muondata

--- train_gpt.py
+++ train_gpt.py
@@ -219,6 +219,93 @@ class Muon(torch.optim.Optimizer):
                 params_world = params[base_i : base_i + self.world_size]
             update_prev()
 
+
+class MuonData(torch.optim.Optimizer):
+    """
+    Muon - MomentUm Orthogonalized by Newton-schulz
+
+    https://kellerjordan.github.io/posts/muon/
+
+    Muon internally runs standard SGD-momentum, and then performs an orthogonalization post-
+    processing step, in which each 2D parameter's update is replaced with the nearest orthogonal
+    matrix. To efficiently orthogonalize each update, we use a Newton-Schulz iteration, which has
+    the advantage that it can be stably run in bfloat16 on the GPU.
+
+    Some warnings:
+    - This optimizer should not be used for the embedding layer, the final fully connected layer,
+    or any {0,1}-D parameters; those should all be optimized by a standard method (e.g., AdamW).
+    - To use it with 4D convolutional filters, it works well to just flatten their last 3 dimensions.
+
+    Arguments:
+        lr: The learning rate used by the internal SGD.
+        momentum: The momentum used by the internal SGD.
+        nesterov: Whether to use Nesterov-style momentum in the internal SGD. (recommended)
+        ns_steps: The number of Newton-Schulz iteration steps to use.
+    """
+    def __init__(self, params, lr=0.02, momentum=0.95, beta2=None, nesterov=True, ns_steps=5, rank=0, world_size=1, eps=1e-8):
+        self.rank = rank
+        self.world_size = world_size
+        defaults = dict(lr=lr, momentum=momentum, nesterov=nesterov, ns_steps=ns_steps, beta2=beta2, eps=eps)
+        params: list[Tensor] = [*params]
+        param_groups = []
+        for size in {p.numel() for p in params}:
+            b = torch.empty(world_size, size, dtype=torch.bfloat16, device="cuda")
+            group = dict(params=[p for p in params if p.numel() == size],
+                         update_buffer=b, update_buffer_views=[b[i] for i in range(world_size)])
+            param_groups.append(group)
+        super().__init__(param_groups, defaults)
+
+    @torch.no_grad()
+    def step(self):
+        for group in self.param_groups:
+            update_buffer: Tensor = group["update_buffer"]
+            update_buffer_views: list[Tensor] = group["update_buffer_views"]
+            # generate weight updates in distributed fashion
+            params: list[Tensor] = group["params"]
+            handle = None
+            params_world = None
+            def update_prev(): # optimized Muon implementation contributed by @YouJiacheng
+                handle.wait()
+                for p_world, g_world in zip(params_world, update_buffer_views):
+                    p_world.data = newtonschulz5(p_world.data) - g_world.view_as(p_world) * (group["lr"] * max(1, p_world.size(-2) / p_world.size(-1))**0.5)
+                    # p_world.add_(g_world.view_as(p_world),
+                                 # alpha=-group["lr"] * max(1, p_world.size(-2) / p_world.size(-1))**0.5)
+            for base_i in range(len(params))[::self.world_size]:
+                if base_i + self.rank < len(params):
+                    p = params[base_i + self.rank]
+                    g = p.grad
+                    assert g is not None
+                    state = self.state[p]
+                    if "momentum_buffer" not in state:
+                        state["momentum_buffer"] = torch.zeros_like(g)
+                    if group['beta2'] is not None and "exp_avg_sq" not in state:
+                        state["exp_avg_sq"] = torch.zeros_like(g)
+                    if 'step' not in state:
+                        state['step'] = 0
+                    state['step'] += 1
+                    buf: Tensor = state["momentum_buffer"]
+                    if group["beta2"] is not None:
+                        exp_avg_sq: Tensor = state["exp_avg_sq"]
+                    buf.lerp_(g, 1 - group["momentum"])
+                    if group['beta2'] is not None:
+                        exp_avg_sq.mul_(group['beta2']).addcmul_(g, g, value=1 - group['beta2'])
+                    g = g.lerp_(buf, group["momentum"]) if group["nesterov"] else buf
+                    #norm_without_new = zeropower_via_newtonschulz5(g, steps=group["ns_steps"]).flatten().norm().item()
+                    if group['beta2'] is not None:
+                        #norm_pre = g.norm().item()
+                        g.div_(exp_avg_sq.sqrt().div_(math.sqrt(1 - group['beta2'] ** state['step'])).add_(group['eps']))
+                        #norm_post1 = g.norm().item()
+                    #pre_update = g.clone()
+                    # g = zeropower_via_newtonschulz5(g, steps=group["ns_steps"]).flatten()
+                    #print(f'norms: {norm_pre} {norm_post1} {norm_without_new} {g.norm().item()} {(pre_update.flatten() - g).norm().item()}')
+                else:
+                    g = update_buffer_views[self.rank]
+                if base_i > 0:
+                    update_prev() # async all_gather instead of sync all_reduce by @YouJiacheng
+                handle = dist.all_gather_into_tensor(update_buffer, g, async_op=True)
+                params_world = params[base_i : base_i + self.world_size]
+            update_prev()
+
 # -----------------------------------------------------------------------------
 # PyTorch nn.Module definitions for the model
 
